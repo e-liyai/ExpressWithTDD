0c8e0fa39e4936470459e65ad94cf1ea017109bb
Andela-eugene <eugene.liyai@andela.com>
2019-08-27 17:58:38 +0300

    feat: add table

--- src/module/User.js
+++ src/module/User.js
@@ -1,5 +1,16 @@
+const pgDatabase = require('../database');
+
+const TABLE_NAME = 'Users';
+
 const up = () => {
-}
+  return pgDatabase.schema.createTableIfNotExists(TABLE_NAME, table => {
+    table.increments()
+    table.string('firstname')
+    table.string('lastname')
+    table.string('location')
+    table.timestamps()
+  });
+};
 
 module.exports = {
   up
